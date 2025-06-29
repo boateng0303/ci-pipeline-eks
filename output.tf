1f8accb759cf5cd0eda88f9fb5601b7d0774d71a
boateng0303 <boatengkwasi0303@gmail.com>
2025-06-29 17:24:28 -0400

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,44 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "vpc_id" {
+  description = "ID of the VPC where the cluster is deployed"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "configure_kubectl" {
+  description = "Configure kubectl command"
+  value       = "aws eks --region ${var.region} update-kubeconfig --name ${aws_eks_cluster.main.name}"
+}
