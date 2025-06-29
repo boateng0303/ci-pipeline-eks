f5ed5a7bce7a1405c7ea94208d19a3bb70cc6f8c
boateng0303 <boatengkwasi0303@gmail.com>
2025-06-29 17:41:38 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -27,3 +27,20 @@ variable "node_instance_types" {
   type        = list(string)
   default     = ["t3.medium"]
 }
+variable "node_desired_size" {
+  description = "Desired number of nodes in the EKS node group"
+  type        = number
+  default     = 2
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in the EKS node group"
+  type        = number
+  default     = 4
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in the EKS node group"
+  type        = number
+  default     = 1
+}
