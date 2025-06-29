77690ccd3743ab868854561f2be00eeb8d1ba616
boateng0303 <boatengkwasi0303@gmail.com>
2025-06-29 17:23:39 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "node_instance_types" {
+  description = "Instance types for worker nodes"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
